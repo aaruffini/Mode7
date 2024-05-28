baee0c46360de8d629c3518dd20e4f931e2e2b09
Alexander Ruffini <aaruffini@gmail.com>
2024-05-28 02:21:47 -0400

    Setup for file, copied code for main.py

---
+++ mode7.py
@@ -0,0 +1,12 @@
+import pygame as PG
+import numpy as np
+from settings import *
+
+class Mode7:
+    def __init__(self,app):
+        self.app = app
+
+    def update(self):
+        pass
+    def draw(self):
+        pass
